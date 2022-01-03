cc241290cb44293a98aa0e1453c6f1043c28cc5a
GriffinCodes <griffin@projecteden.gg>
2022-01-02 23:36:29 -0600

    Update number generator

--- src/main/java/gg/projecteden/tools/Numbers.java
+++ src/main/java/gg/projecteden/tools/Numbers.java
@@ -39,47 +39,33 @@ public class Numbers {
 		}
 	}
 
-	private enum Color {
-		GREEN,
-		RED,
-		GRAY,
-		CYAN
-	}
-
 	private static final StringBuilder script = new StringBuilder();
 
 	@Test
 	@SneakyThrows
 	void generate() {
-		for (Color colorType : Color.values()) {
-			String color = colorType.name().toLowerCase();
+		final String generated = "generated/";
+		exec("mkdir -p " + generated);
 
-			final String colorFolder = "generated/" + color;
-			exec("mkdir -p " + colorFolder);
+		for (int i = 0; i < 10; i++) {
+			String file = generated + i + ".json";
+			exec("cp template_single.json " + file);
+			exec("sed -i 's/__UP__/" + UVTemplate.of(i, 0) + "/g' " + file);
+		}
 
-			for (int i = 0; i < 10; i++) {
-				String file = colorFolder + "/" + i + ".json";
-				exec("cp template_single.json " + file);
-				exec("sed -i 's/__COLOR__/" + color + "/g' " + file);
-				exec("sed -i 's/__UP__/" + UVTemplate.of(i, 0) + "/g' " + file);
-			}
+		for (int i = 10; i < 100; i++) {
+			String file = generated + i + ".json";
+			exec("cp template_double.json " + file);
+			exec("sed -i 's/__UP_LEFT__/" + UVTemplate.of(i, 0) + "/g' " + file);
+			exec("sed -i 's/__UP_RIGHT__/" + UVTemplate.of(i, 1) + "/g' " + file);
+		}
 
-			for (int i = 10; i < 100; i++) {
-				String file = colorFolder + "/" + i + ".json";
-				exec("cp template_double.json " + file);
-				exec("sed -i 's/__COLOR__/" + color + "/g' " + file);
-				exec("sed -i 's/__UP_LEFT__/" + UVTemplate.of(i, 0) + "/g' " + file);
-				exec("sed -i 's/__UP_RIGHT__/" + UVTemplate.of(i, 1) + "/g' " + file);
-			}
-
-			for (int i = 100; i < 1000; i++) {
-				String file = colorFolder + "/" + i + ".json";
-				exec("cp template_triple.json " + file);
-				exec("sed -i 's/__COLOR__/" + color + "/g' " + file);
-				exec("sed -i 's/__UP_LEFT__/" + UVTemplate.of(i, 0) + "/g' " + file);
-				exec("sed -i 's/__UP_CENTER__/" + UVTemplate.of(i, 1) + "/g' " + file);
-				exec("sed -i 's/__UP_RIGHT__/" + UVTemplate.of(i, 2) + "/g' " + file);
-			}
+		for (int i = 100; i < 1000; i++) {
+			String file = generated + i + ".json";
+			exec("cp template_triple.json " + file);
+			exec("sed -i 's/__UP_LEFT__/" + UVTemplate.of(i, 0) + "/g' " + file);
+			exec("sed -i 's/__UP_CENTER__/" + UVTemplate.of(i, 1) + "/g' " + file);
+			exec("sed -i 's/__UP_RIGHT__/" + UVTemplate.of(i, 2) + "/g' " + file);
 		}
 
 		Files.write(Paths.get(PATH + "/generate.sh"), script.toString().getBytes(), CREATE, TRUNCATE_EXISTING);
